918a345446709e2dd12d7af00325688a36f7ac48
Thom David <thomdavid74@gmail.com>
2018-03-17 11:45:10 -0700

    Variables defined

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define UI variables
+const form = document.querySelector('#task-form'); // Task entry form 
+const taskList = document.querySelector('.collection'); // List of tasks to be modified
+const clearBtn = document.querySelector('.clear-tasks'); // Clear task functionality hook
+const filter = document.querySelector('#filter'); // Filter tasks functionality hook
+const taskInput = document.querySelector('#task'); // Task input value field hook
