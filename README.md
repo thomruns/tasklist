296f166f02a07c79ab242458daad50c7df698615
Thom David <thomdavid74@gmail.com>
2018-03-17 14:39:33 -0700

    Filter task functionality

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ Basic Tasklist App using Materialize as CSS but only vanilla Javascript (jQuery 
 Basic functionality for adding tasks to a task list.
 
 ## Functionality for deleting tasks
-Basic functionality for deleting tasks from the task list by clicking an icon.
+Basic functionality for deleting individual tasks from the task list by clicking an icon.
 
 ## Modifications date
 Reference for date of modifications; may be deleted upon app completion
 
 ## Task deletion functionality
 Functionality for clearing all tasks from list
+
+## Task filter functionality
+Functionality for filtering the task list
--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // addTask FUNCTION
@@ -77,7 +79,21 @@ function clearTasks() {
   while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
-}
+} // end clearTasks function
+
+// filterTasks function
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+} // end filterTasks function
 
 // Reference for modifications only -- may be deleted
 const modifiedDate = document.lastModified;
